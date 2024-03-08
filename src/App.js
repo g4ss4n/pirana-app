d279edc1f616afbcb12660f706ef811f6f240387
Khaled <khaled.shahbari@gmail.com>
2024-03-08 14:56:36 +0200

    as

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import MultiqueryPir from './components/multiqueryPir/MultiqueryPir';
 import BatchPir from './components/batchPir/BatchPir';
 import LabeledPsi from './components/labeledPsi/LabeledPsi';
 import Pirana from './components/pirana/Pirana';
-
+import SingleQuerySP from './components/singleQuerySP/SingleQuerySP';
 
 
 function App() {
@@ -80,6 +80,8 @@ function App() {
         return <LabeledPsi />;
       case 'pirana':
         return <Pirana />;
+      case 'single-query-small-payloads':
+        return <SingleQuerySP />;
       // Add cases for other components as needed
       default:
         return null;
