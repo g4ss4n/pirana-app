f5f1da902d54c5d8746469a6c0dd7c40b1efc490
Khaled <khaled.shahbari@gmail.com>
2024-03-11 01:04:11 +0200

    aa

--- src/components/singleQuerySP/SingleQuerySP.js
+++ src/components/singleQuerySP/SingleQuerySP.js
@@ -10,14 +10,14 @@ const SingleQuerySP = () => {
                 &nbsp; &nbsp; &nbsp;
                 r := i<sup>*</sup>%N
                 &nbsp; &nbsp; &nbsp;
-                c := ⌈i<sup>*</sup>/N⌉             </text>, explanation: <text>t = 3 &nbsp; &nbsp; r = 0 &nbsp; &nbsp; c = 1 <br />
-                The client calculates that Element 3 is in row 0 and column 1.</text>
+                c := ⌈i<sup>*</sup>/N⌉             </text>, explanation: <text>t = 3 &nbsp; &nbsp; r = 1 &nbsp; &nbsp; c = 2 <br />
+                The client calculates that Element 2 is in row 1 and column 2.</text>
         },
         {
             code: <text>x ∈ CW(m, k) ← run Algorithm 1 (Mapping indices to constant-weight codewords) with c ∈ [1, ..., t]            <br />
                 Find the k positions [i<sub>1</sub>, ..., i<sub>k</sub>] in x, where x[i] = 1             </text>, 
-                explanation: <text> encodes the column index c = 1 into a constant- weight codeword x, for example CW(4, 2): <br />&rarr; x = 0110.< br />< br />
-Then, we find the k positions in CW codeword that has 1     </text >
+                explanation: <text> Encodes the column index c = 2 into a constant- weight codeword x, for example CW(5, 2): <br />&rarr; x = 00011.< br />< br />
+Then, we find the k positions in CW codeword that has 1, which are i<sub>4</sub> and i<sub>5</sub>.</text >
         },
 
         {
@@ -29,14 +29,16 @@ Then, we find the k positions in CW codeword that has 1     </text >
                 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;q<sub>j</sub> [h] := 0 <br />
                 &nbsp; &nbsp; &nbsp; qej ← SIMDEnc(q<sub>j</sub>)
             </text>,
-            explanation: <text>Based on x and the row index r = 0, the client generates SIMD ciphertexts. Let's say these ciphertexts are:<br />
+            explanation: <text>Based on x and the row index r = 1, the client generates SIMD ciphertexts. Let's say these ciphertexts are:<br />
                 <ol>
-                    <li>SIMD Ciphertext qe0: SIMDEnq([0, 0, 0, 0])</li>  
-                    <li>SIMD Ciphertext qe1: SIMDEnq([1, 0, 0, 0])</li>  
-                    <li>SIMD Ciphertext qe2: SIMDEnq([1, 0, 0, 0])</li>  
-                    <li>SIMD Ciphertext qe3: SIMDEnq([0, 0, 0, 0])</li>  
+                    <li>SIMD Ciphertext qe<sub>1</sub>: SIMDEnq([0, 0, 0])</li> 
+                    <li>SIMD Ciphertext qe<sub>2</sub>: SIMDEnq([0, 0, 0])</li> 
+                    <li>SIMD Ciphertext qe<sub>3</sub>: SIMDEnq([0, 0, 0])</li>  
+                    <li>SIMD Ciphertext qe<sub>4</sub>: SIMDEnq([1, 0, 0])</li>  
+                    <li>SIMD Ciphertext qe<sub>5</sub>: SIMDEnq([1, 0, 0])</li>  
+                     
                 </ol>
-The client send qe=[qe0, qe1, qe2, qe3] to server            </text>
+The client send qe=[qe<sub>1</sub>, qe<sub>2</sub>, qe<sub>3</sub>, q3<sub>4</sub>, qe<sub>5</sub>] to server            </text>
         },
 
     ]
@@ -56,18 +58,18 @@ The client send qe=[qe0, qe1, qe2, qe3] to server            </text>
                 &nbsp; &nbsp; &nbsp; yj ∈ CW(m, k) ← run Algorithm 1 with j ∈[t]
 
             </text>,
-            explanation: <text>We encode the column indices j to a constant-weight codewords y<sub>j</sub>, so at the end of the loop we get t constant-weight codewords [y<sub>1</sub>, ..., y<sub>t</sub>], that encodes the column indices [1, ..., t].
+            explanation: <text>We encode the column indices j to a constant-weight codewords y<sub>j</sub>, at the end of the loop we get 3 constant-weight codewords [y<sub>1</sub>, y<sub>2</sub>, y<sub>3</sub>], that encodes the column indices [1, 2, 3].
             </text>
         },
         {
             code: <text>&nbsp; &nbsp; &nbsp; Find k positions [i<sub>1</sub>, ..., i<sub>k</sub>] in y<sub>j</sub> , where y<sub>j</sub>[i] = 1<br />
                         &nbsp; &nbsp; &nbsp; wej ← SIMDMul(qe<sub>i1</sub>, ..., qe<sub>ik</sub>) <br />
                         </text>,
-            explanation: <text>It performs equality operations on the SIMD ciphertexts received from the client and the constant-weight codewords y<sub>j</sub>, the result is t SIMD ciphertexts (3 ciphertexts in this example), where only the r-th slot (which is 0 in this example) is 1 and all other slots are 0s.<br />
+            explanation: <text>It performs equality operations on the SIMD ciphertexts received from the client and the constant-weight codewords y<sub>j</sub>, the result is t SIMD ciphertexts (3 ciphertexts in this example), where only the r-th slot (which is 1 in this example) is 1 and all other slots are 0s.<br />
             <ol>
-                <li>we0:  SIMD ciphertext([1, 0, 0, 0])</li>
-                <li>we1:  SIMD ciphertext([1, 0, 0, 0])</li>
-                <li>we2:  SIMD ciphertext([1, 0, 0, 0])</li>
+                <li>we<sub>1</sub>:  SIMD ciphertext([0, 0, 0])</li>
+                <li>we<sub>2</sub>:  SIMD ciphertext([1, 0, 0])</li>
+                <li>we<sub>3</sub>:  SIMD ciphertext([0, 0, 0])</li>
             </ol>
             Note that the server has now the same t SIMD ciphertexts as in the aforementioned naive solution.
             </text>
@@ -76,9 +78,9 @@ The client send qe=[qe0, qe1, qe2, qe3] to server            </text>
             code: <text>&nbsp; &nbsp; &nbsp; wej ← SIMDPmul(we<sub>j</sub>, d<sub>j</sub>)</text>,
             explanation: <text>We multiplies the j-th column of the database (d<sub>j</sub>) to we<sub>j</sub>.<br />
                 <ol>
-                    <li>ue0:  SIMD ciphertext([Element 0, 0, 0, 0])</li>
-                    <li>ue1:  SIMD ciphertext([Element 3, 0, 0, 0])</li>
-                    <li>ue2:  SIMD ciphertext([Element 6, 0, 0, 0])</li>
+                    <li>ue<sub>1</sub>:  SIMD ciphertext([0, 0, 0])</li>
+                    <li>ue<sub>2</sub>:  SIMD ciphertext([Element 2, 0, 0])</li>
+                    <li>ue<sub>3</sub>:  SIMD ciphertext([0, 0, 0])</li>
                 </ol>
             </text>
         },
@@ -86,12 +88,12 @@ The client send qe=[qe0, qe1, qe2, qe3] to server            </text>
             code: <text>ve ← SIMDAdd(ue<sub>1</sub>, ..., ue<sub>t</sub>)
             </text>,
             explanation: <text>We adds the products of all columns together<br />
-                ve: <br /> SIMD ciphertext([Element 0, Element 3, Element 6])
+                ve: <br /> SIMD ciphertext([Element 2, 0, 0])
             </text >
         },
         {
             code: <text>return ve</text>,
-            explanation: <text> The server sends ve to the client, which decrypts ve, and output the payload based on the column encrypted in x, identifying the payload associated with Element 3.</text >
+            explanation: <text> The server sends ve to the client, which decrypts ve, output the payload on the r-th slot (which is 1 row), identifying the payload associated with Element 2.</text >
         },
 
     ];
@@ -118,14 +120,14 @@ The client send qe=[qe0, qe1, qe2, qe3] to server            </text>
         Parameters for homomorphic encryption are chosen, and keys are generated. S arranges its database
         into a matrix of N rows and t := n/N columns.<br /> <br/>
         Lets assume we have 9 element in th database, the server arranges its database into a matrix with 3 rows and 3 columns: <br /> <br />
-                    Element 0 &nbsp;&nbsp;&nbsp;&nbsp; <strong>Element 3</strong> &nbsp;&nbsp;&nbsp;&nbsp; Element 6<br />
-                Element 1 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Element 4 &nbsp;&nbsp;&nbsp;&nbsp; Element 7<br />
-                    Element 2 &nbsp;&nbsp;&nbsp;&nbsp; Element 5 &nbsp;&nbsp;&nbsp;&nbsp; Element 8<br /><br />
+                    Element 1 &nbsp;&nbsp;&nbsp;&nbsp; <strong>Element 2</strong> &nbsp;&nbsp;&nbsp;&nbsp; Element 3<br />
+                Element 4 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Element 5 &nbsp;&nbsp;&nbsp;&nbsp; Element 6<br />
+                    Element 7 &nbsp;&nbsp;&nbsp;&nbsp; Element 8 &nbsp;&nbsp;&nbsp;&nbsp; Element 9<br /><br />
                     N = 3 &nbsp; &nbsp;&nbsp; (number of rows)<br />
-                    m = 4 &nbsp; &nbsp; (number of slots in the ciphertext)<br />
+                    m = 5 &nbsp; &nbsp; (number of slots in the ciphertext)<br />
                     k = 2 &nbsp; &nbsp; &nbsp; (weight of the constant-weight codeword)<br />
                     n = 9 &nbsp; &nbsp; &nbsp; (total number of elements in the database)<br /><br />
-                    Let i<sup>*</sup> be the 3rd element
+                    Let i<sup>*</sup> be the 2nd element
 
                 
         </p>
